3be7e8ba85582f2800e7c6bfad26c3f225858aba
Dean-jay <80184397+Dean-jay@users.noreply.github.com>
2021-09-03 23:04:32 +1000

    Binding new list

--- js/app.js
+++ js/app.js
@@ -6,16 +6,15 @@
  */
 
 const addBtn = document.querySelector('.button');
-const checkbox = document.querySelector('.checkbox');
 const todayjob = document.querySelector('.task');
 const textcontainer = document.querySelector('.input__text');
 const incompletedHolder = document.querySelector('.incompleted__task');
 
 function addText() {
-  console.log(textcontainer.value);
+  if (textcontainer.value == '') return;
   const listoftask = createHTMLList(textcontainer.value);
-  console.log(listoftask);
   incompletedHolder.appendChild(listoftask);
+  bindTaskEvents(listoftask);
   textcontainer.value = '';
 }
 
@@ -23,17 +22,45 @@ function createHTMLList(textTask) {
   const listItem = document.createElement('li');
   const checkbox = document.createElement('input');
   const label = document.createElement('p');
+  const dBtn = document.createElement('button');
+  const eBtn = document.createElement('button');
 
+  listItem.className = 'checkbox_list';
   checkbox.type = 'checkbox';
   checkbox.className = 'checkbox';
   label.innerText = textTask;
   label.className = 'task';
-  listItem.className = 'checkbox_list';
+  dBtn.innerText = 'delete';
+  dBtn.className = 'delete';
+  eBtn.innerText = 'edit';
+  eBtn.className = 'edit';
 
   listItem.appendChild(checkbox);
   listItem.appendChild(label);
+  listItem.appendChild(dBtn);
+  listItem.appendChild(eBtn);
   return listItem;
 }
+
+function editTask() {
+  console.log('eidt');
+}
+function deleteTask() {
+  console.log('delete');
+}
+function checkBoxEventHandler() {
+  console.log('finish');
+}
+
+const bindTaskEvents = function (taskListItem) {
+  const checkbox = taskListItem.querySelector('.checkbox');
+  const deleteBtn = taskListItem.querySelector('.delete');
+  const editBtn = taskListItem.querySelector('.edit');
+  console.log(editBtn);
+  editBtn.onclick = editTask;
+  deleteBtn.onclick = deleteTask;
+  checkbox.onclick = checkBoxEventHandler;
+};
 
 addBtn.addEventListener('click', () => addText());
 textcontainer.addEventListener('keypress', function (e) {
@@ -41,3 +68,7 @@ textcontainer.addEventListener('keypress', function (e) {
     addText();
   }
 });
+
+// 새로 생긴 리스트에 js가 적용이 안된다.
+// binding issue : 이벤트가 처리되는 함수 안에서 queryseclector을 써준다.
+// list에 빈 input 을 같이 주는 이유는 edit을 위해서 이다.
